f9c8b64eab8fe9ce7b405cf780cb1f47d0190797
JesseAger <agerjesse6@gmail.com>
2025-06-18 23:14:06 +0300

    settings-and-conf

--- notifications/models.py
+++ notifications/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from users.models import CustomUser
+# from users.models import CustomUser
 from tickets.models import tickets
-
+from django.conf import settings
 
 
 class notifications(models.Model):
-    staff = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    staff = models.ForeignKey(settings.AUTH_USER_MODELS, on_delete=models.CASCADE)
     ticket = models.ForeignKey(tickets, on_delete=models.CASCADE)
     message = models.CharField(max_length=255)
     is_read = models.BooleanField(default=False)
--- tickets/models.py
+++ tickets/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from users.models import CustomUser
-
+# from users.models import CustomUser
+from django.conf import settings
 class tickets(models.Model):
     STATUS_CHOICES =(
         ('PENDING', "Pending"),
@@ -13,14 +13,14 @@ class tickets(models.Model):
     ticket_number = models.CharField(max_length=20, unique=True)
     description = models.TextField()
     title = models.CharField(max_length=100)
-    reported_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name= 'reported_tickets')
-    assigned_to =  models.ForeignKey(CustomUser, on_delete=models.SET_NULL, blank=True, null=True, related_name='assigned_tickets')
+    reported_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name= 'reported_tickets')
+    assigned_to =  models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True, related_name='assigned_tickets')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
 class TicketComment(models.Model):
     ticket = models.ForeignKey(tickets, on_delete=models.SET_NULL, blank=True, null= True, related_name='comment')
-    author = models. ForeignKey(CustomUser, on_delete=models.CASCADE)
+    author = models. ForeignKey(settings.AUTH_USER_MODELS, on_delete=models.CASCADE)
     comment = models.TextField()
     timestamp =  models.DateTimeField(auto_now_add=True)
     
