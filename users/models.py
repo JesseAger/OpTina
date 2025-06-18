2fa1bb34069fe4e446cf3b3d6b308c2cadad26f6
JesseAger <agerjesse6@gmail.com>
2025-06-18 22:00:53 +0300

    roles

--- users/models.py
+++ users/models.py
@@ -1,18 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class user(models.Model):
-
-
-    ROLE_CHOICES=(
-        ('STAFF', "staff"),
-        ('ADMIN', "System Admin"),
-        ('SUPPORT', "IT Support")
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('STAFF', 'Staff'),
+        ('ADMIN', 'System Admin'),
+        ('SUPPORT', 'IT Support'),
     )
 
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    email = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=10, unique=True)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-    password = models.CharField(max_length=20, unique=True)
 
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name', 'phone_number', 'role']
